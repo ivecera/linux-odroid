c8d74e8c2fac1dff418faf4dcbc98d746e1bba30
Ivan Vecera <ivan@cera.cz>
2021-04-30 15:42:07 +0200

    techpack: audio: tas2557: simplify debug logging using dev_dbg
    Use dev_dbg instead of:
    ...
     #ifdef DEBUG
     dev_info(...)
     #endif
    ...
    
    Change-Id: I5960af7127d973318950cccf1ddc60cc21a34307
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- techpack/audio/asoc/codecs/tas2557/tiload.c
+++ techpack/audio/asoc/codecs/tas2557/tiload.c
@@ -101,9 +101,6 @@ static ssize_t tiload_read(struct file *filp, char __user *buf,
 	char reg_addr;
 	size_t size;
 	int ret = 0;
-#ifdef DEBUG
-	/* int i; */
-#endif
 
 	dev_info(pTAS2557->dev, "%s\n", __func__);
 	if (count > MAX_LENGTH) {
@@ -135,14 +132,9 @@ static ssize_t tiload_read(struct file *filp, char __user *buf,
 		dev_err(pTAS2557->dev, "%s, %d, ret=%d, count=%zu error happen!\n",
 			__func__, __LINE__, ret, count);
 
-#ifdef DEBUG
-	dev_info(pTAS2557->dev, "read size = %d, reg_addr= %x , count = %d\n",
-		(int) size, reg_addr, (int) count);
-/*	for (i = 0; i < (int) size; i++) {
-*		dev_dbg(pTAS2557->dev, "rd_data[%d]=%x\n", i, rd_data[i]);
-*	}
-*/
-#endif
+	dev_dbg(pTAS2557->dev, "read size = %zd, reg_addr= %x , count = %zd\n",
+		size, reg_addr, count);
+
 	if (size != count)
 		dev_err(pTAS2557->dev, "read %d registers from the codec\n", (int) size);
 
@@ -171,9 +163,7 @@ static ssize_t tiload_write(struct file *filp, const char __user *buf,
 	unsigned int nCompositeRegister = 0;
 	unsigned int nRegister;
 	int ret = 0;
-#ifdef DEBUG
-	/* int i; */
-#endif
+
 	dev_info(pTAS2557->dev, "%s\n", __func__);
 
 	if (count > MAX_LENGTH) {
@@ -187,13 +177,9 @@ static ssize_t tiload_write(struct file *filp, const char __user *buf,
 		dev_err(pTAS2557->dev, "copy_from_user failure %d\n", (int) size);
 		return -EINVAL;
 	}
-#ifdef DEBUG
-	dev_info(pTAS2557->dev, "write size = %zu\n", count);
-/* for (i = 0; i < (int) count; i++) {
-*		dev_info(pTAS2557->dev, "wr_data[%d]=%x\n", i, wr_data[i]);
-*	}
-*/
-#endif
+
+	dev_dbg(pTAS2557->dev, "write size = %zu\n", count);
+
 	nRegister = wr_data[0];
 	size = count;
 	if ((nRegister == 127) && (gPage == 0)) {
